cf2b71446806524f22c98e315c3385f72f6405b6
Abhinav Tushar <abhinav@lepisma.xyz>
2020-09-27 11:38:27 +0530

    fortnightly script

---
+++ scripts/fortnightly.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+week=$(date +%V)
+
+if [[ $((week % 2)) -eq 0 ]];
+then
+    echo "Week number $week is even. Skipping.";
+else
+    poetry run ccb group --output-json=matches.json
+    poetry run ccb post --matches-json=matches.json --channel-name=all-them-bots
+fi
