34ef584c56515b926bfa65f2e5db3350c02a4da9
Abhinav Tushar <abhinav@lepisma.xyz>
2020-09-27 11:40:15 +0530

    test for condition

--- scripts/fortnightly.sh
+++ scripts/fortnightly.sh
@@ -4,8 +4,8 @@ week=$(date +%V)
 
 if [[ $((week % 2)) -eq 0 ]];
 then
-    echo "Week number $week is even. Skipping.";
-else
     poetry run ccb group --output-json=matches.json
     poetry run ccb post --matches-json=matches.json --channel-name=all-them-bots
+else
+    echo "Week number $week is even. Skipping.";
 fi
